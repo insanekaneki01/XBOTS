c5bd86b35622ad728bb0cce04afd5dab1308dd20
insanekaneki01 <158169677+insanekaneki01@users.noreply.github.com>
2024-02-15 14:08:32 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,7 +8,7 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/INSAME_NETWORK"),
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/INSANE_NETWORK"),
         Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/Insanesociety")
     ],
     [
