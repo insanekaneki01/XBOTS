83a8d65ee5dc3c3ed1a8112edf031295502e5061
insanekaneki01 <158169677+insanekaneki01@users.noreply.github.com>
2024-02-28 15:37:19 +0530

    Update spam.py

--- AltBots/modules/spam.py
+++ AltBots/modules/spam.py
@@ -80,7 +80,7 @@ async def spam(event: events):
 async def pspam(event):
     if event.sender_id in SUDO_USERS:
         if event.chat_id in GROUP:
-            await event.reply("» YE GROUP ASHWANI KE UNDER MAI HAI ISLEYE ISME PSPAM NHI HOGA...")
+            await event.reply("» YE GROUP KANEKI KE ANDER ME HAI ISLEYE ISME PSPAM NHI HOGA...")
         else:
             try:
                 counter = int(event.text.split(" ", 2)[1])
